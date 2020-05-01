61d622a148ced456f8c5dcf3265da3ca706e0988
Dagrea <dagonsha@gmail.com>
2020-05-01 14:27:45 +0300

    some ground

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      yourBoard: Array(100).fill("hide"),
+      enemyBoard: Array(100).fill("hide"),
+      yourShips: Array(100).fill(null),
+      enemyShips: Array(100).fill(null)
+    }
+  }
+ 
+	handleClick(index) {
+		
+		if(this.state.enemyBoard[index] == "hide") {
+			let newBoard = this.state.enemyBoard;
+            newBoard[index] = "miss";
+            this.setState({
+            	enemyBoard: newBoard
+            })
+            return
+        }
+        if(this.state.enemyBoard[index] == "miss") {
+			let newBoard = this.state.enemyBoard;
+            newBoard[index] = "alive";
+            this.setState({
+            	enemyBoard: newBoard
+            })
+            return
+        }
+        if(this.state.enemyBoard[index] == "alive") {
+			let newBoard = this.state.enemyBoard;
+            newBoard[index] = "hit";
+            this.setState({
+            	enemyBoard: newBoard
+            })
+        }
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  renderYCells() {
+    return this.state.yourBoard.map(
+      (cell, index) =>
+        <div className="cell " key={index}></div>
+    )
+  }
+
+  renderECells() {
+    return this.state.enemyBoard.map(
+      (cell, index) =>
+        this.state.enemyBoard[index] == "hide" ? 
+        (<div className="cell" key={index} onClick={() => this.handleClick(index)}></div>) : 
+        this.state.enemyBoard[index] == "miss" ?
+        (<div className="cell miss" key={index} onClick={() => this.handleClick(index)}></div>) : 
+        this.state.enemyBoard[index] == "alive" ?
+        (<div className="cell alive" key={index} onClick={() => this.handleClick(index)}></div>) :
+        (<div className="cell hit" key={index} onClick={() => this.handleClick(index)}></div>)
+    )
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Naval Battle App</h1>
+        <div className="yourboard">
+        <div className="text">Ваша доска</div>
+          {this.renderYCells()}
+        </div>
+        <div className="enemyboard">
+        <div className="text">Доска противника</div>
+          {this.renderECells()}
+        </div>
+        <div className="footer"></div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
