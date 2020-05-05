5bb11ea24ecdb8abf50ba1d2d1128e474a457216
Dagrea <dagonsha@gmail.com>
2020-05-05 23:02:39 +0300

    added killing

--- src/components/battle.js
+++ src/components/battle.js
@@ -25,16 +25,13 @@ constructor(props) {
       for (let j = 0; j < this.state.yourBoard.length; j++) {
         board[j] = (this.state.yourBoard[j].map(
           (cell,index) =>
-          this.state.yourBoard[j][index] === null ? 
+          this.state.yourBoard[j][index][0] === null || this.state.yourBoard[j][index][0] === undefined ? 
           <div className="cell " key={index}></div> :
-          this.state.yourBoard[j][index] === "hide" ? 
+          this.state.yourBoard[j][index][0] === "hide" ? 
           <div className="cell alive" key={index}></div> :
-          this.state.yourBoard[j][index] === "miss" ? 
+          this.state.yourBoard[j][index][0] === "miss" ? 
           <div className="cell miss" key={index}></div> :
           <div className="cell hit" key={index}></div>
-
-
-
           ));
       }
       return board;
@@ -45,11 +42,11 @@ constructor(props) {
       for (let j = 0; j < this.state.enemyBoard.length; j++) {
         board[j] = (this.state.enemyBoard[j].map(
           (cell,index) =>
-          this.state.enemyBoard[j][index] === null ? 
+          this.state.enemyBoard[j][index][0] === null || this.state.enemyBoard[j][index][0] === undefined ? 
           <div className="cell" key={index} onClick={() => this.handleClick(j+index.toString())}></div> :
-          this.state.enemyBoard[j][index] === "hide" ? 
+          this.state.enemyBoard[j][index][0] === "hide" ? 
           <div className="cell" key={index} onClick={() => this.handleClick(j+index.toString())}></div> :
-          this.state.enemyBoard[j][index] === "miss" ? 
+          this.state.enemyBoard[j][index][0] === "miss" ? 
           <div className="cell miss" key={index} onClick={() => this.handleClick(j+index.toString())}></div> :
           <div className="cell hit" key={index} onClick={() => this.handleClick(j+index.toString())}></div>
           )
@@ -62,18 +59,21 @@ constructor(props) {
     let firstChar = Number(index.toString().slice(0,1));
     let secondChar = Number(index.toString().slice(-1));
     let newBoard = this.state.enemyBoard;
-    if (this.state.enemyBoard[firstChar][secondChar] === "hide"){
-      newBoard[firstChar][secondChar] = "kill";
+    if (this.state.enemyBoard[firstChar][secondChar][0] === "kill" || this.state.enemyBoard[firstChar][secondChar][0] === "miss") {return false}
+    if (this.state.enemyBoard[firstChar][secondChar][0] === "hide"){
+      newBoard[firstChar][secondChar][0] = "kill";
+      this.checkKill(this.state.enemyBoard[firstChar][secondChar][1]);
       this.setState({
         enemyBoard: newBoard,
       })
       return
     }
-    newBoard[firstChar][secondChar] = "miss";
-      this.setState({
-        enemyBoard: newBoard,
-      })
-    this.enemyShot()
+    newBoard[firstChar][secondChar] = Array(2);
+    newBoard[firstChar][secondChar][0] = "miss";
+    this.setState({
+      enemyBoard: newBoard,
+    })
+    this.enemyShot();
   }
 
   enemyShot() {
@@ -83,14 +83,8 @@ constructor(props) {
     let secondChar = Number(target.toString().slice(-1));
     if (this.state.enemyShots.includes(target)) {this.enemyShot();return}
     let newBoard = this.state.yourBoard;
-    if (this.state.yourBoard[firstChar][secondChar] === null) {
-      newBoard[firstChar][secondChar] = "miss";
-      this.setState({
-        yourBoard: newBoard,
-      })
-    }
-    if (this.state.yourBoard[firstChar][secondChar] === "hide") {
-      newBoard[firstChar][secondChar] = "kill";
+    if (this.state.yourBoard[firstChar][secondChar][0] === "hide") {
+      newBoard[firstChar][secondChar][0] = "kill";
       let shots = this.state.enemyShots;
       shots.push(target);
       this.setState({
@@ -98,6 +92,104 @@ constructor(props) {
         enemyShots:shots
       })
       this.enemyShot();
+      return
+    }
+    newBoard[firstChar][secondChar] = Array(2);
+    newBoard[firstChar][secondChar][0] = "miss";
+    let shots = this.state.enemyShots;
+    shots.push(target);
+    this.setState({
+       yourBoard: newBoard,
+      enemyShots:shots
+    })
+  }
+
+  checkKill(ship) {
+    let count = 0;
+    for (let i = 0; i < ship.length; i++) {
+      let firstChar = Number(ship[i].toString().slice(0,1));
+      let secondChar = Number(ship[i].toString().slice(-1));
+      if (ship[i] < 10) {firstChar = 0}
+      if (this.state.enemyBoard[firstChar][secondChar][0] === "kill") {count++}
+    }
+    if (count === ship.length) {
+      let newBoard = this.state.enemyBoard;
+      for (let i = 0; i < ship.length; i++) {
+        let firstChar = Number(ship[i].toString().slice(0,1));
+        let secondChar = Number(ship[i].toString().slice(-1));
+        if (ship[i] < 10) {firstChar = 0}
+//------------------------------------------------------------------------------------
+        if (ship[i] < 10) {
+          if (ship[i] === 0) {
+            for (let j = 0; j < 2; j++) {
+              for (let k = 0; k < 2; k++) {
+                let cur = secondChar+j*10+k;
+                if (ship.includes(cur)) {continue}
+                newBoard[firstChar+j][secondChar+k] = Array(2);
+                newBoard[firstChar+j][secondChar+k][0] = "miss" 
+              }
+            }
+            continue
+          }
+          if (ship[i] === 9) {
+            for (let j = 0; j < 2; j++) {
+              for (let k = -1; k < 1; k++) {
+                let cur = secondChar+j*10+k;
+                if (ship.includes(cur)) {continue}
+                newBoard[firstChar+j][secondChar+k] = Array(2);
+                newBoard[firstChar+j][secondChar+k][0] = "miss" 
+              }
+            }
+          continue
+          }
+          for (let j = 0; j < 2; j++) {
+            for (let k = -1; k < 2; k++) {
+              let cur = secondChar+j*10+k;
+              if (ship.includes(cur)) {continue}
+              newBoard[firstChar+j][secondChar+k] = Array(2);
+              newBoard[firstChar+j][secondChar+k][0] = "miss" 
+            }
+          }
+          continue
+        }
+        if (secondChar === 0) {
+          for (let j = -1; j < 2; j++) {
+            for (let k = 0; k < 2; k++) {
+              let cur = (firstChar+j)*10+(secondChar+k);
+              if (ship.includes(cur)) {continue}
+              if (cur > 99) {continue}
+              newBoard[firstChar+j][secondChar+k] = Array(2);
+              newBoard[firstChar+j][secondChar+k][0] = "miss" 
+            }
+          }
+          continue
+        }
+        if (secondChar === 9) {
+          for (let j = -1; j < 2; j++) {
+            for (let k = -1; k < 1; k++) {
+              let cur = (firstChar+j)*10+(secondChar+k);
+              if (ship.includes(cur)) {continue}
+              if (cur > 99) {continue}
+              newBoard[firstChar+j][secondChar+k] = Array(2);
+              newBoard[firstChar+j][secondChar+k][0] = "miss" 
+            }
+          }
+          continue
+        }
+//------------------------------------------------------------------------------------
+        for (let j = -1; j < 2; j++) {
+          for (let k = -1; k < 2; k++) {
+            let cur = (firstChar+j)*10+(secondChar+k);
+            if (ship.includes(cur)) {continue}
+            if (cur > 99) {continue}
+            newBoard[firstChar+j][secondChar+k] = Array(2);
+            newBoard[firstChar+j][secondChar+k][0] = "miss" 
+          }
+        }
+      }
+      this.setState({
+        enemyBoard: newBoard,
+      })
     }
   }
 
--- src/components/early.js
+++ src/components/early.js
@@ -3,8 +3,8 @@ export default class Early extends React.Component {
 constructor(props) {
     super(props)
     this.state = {
-      yourBoard: Array(10).fill(Array(10).fill(null)),
-      enemyBoard: Array(10).fill(Array(10).fill(null)),
+      yourBoard: Array(10).fill(Array(10).fill(Array())),
+      enemyBoard: Array(10).fill(Array(10).fill(Array())),
       ships: [4,3,3,2,2,2,1,1,1,1],
       next:"battle"
     }
@@ -14,24 +14,18 @@ constructor(props) {
       for (var j = 0; j < this.state.yourBoard.length; j++) {
         board[j] = (this.state.yourBoard[j].map(
           (cell,index) =>
-          this.state.enemyBoard[j][index] === "hide" ? 
+          this.state.enemyBoard[j][index][0] === "hide" ? 
           <div className="cell alive" key={index} ></div> :
           <div className="cell " key={index}></div>))
       }
       return board;
   }
-  reset() {
-    this.setState({
-      enemyBoard: Array(10).fill(Array(10).fill(null)),
-      yourBoard: Array(10).fill(Array(10).fill(null))
-    })
-    }
-    
+
   randomShip(size,newBoard) {
     let startOfShip = Math.floor(Math.random()*100);
     let direction = Math.floor(Math.random()*2);
     if (startOfShip < 10 ) {
-      if (newBoard[0][startOfShip] != null) {newBoard = this.randomShip(size,newBoard);return newBoard}
+      if (newBoard[0][startOfShip][0] != null) {newBoard = this.randomShip(size,newBoard);return newBoard}
       if (direction === 1 && startOfShip > 10-size) {newBoard = this.randomShip(size,newBoard);return newBoard}
       let ship = [];
       for (let i = 0; i < size; i++) {
@@ -47,13 +41,13 @@ constructor(props) {
         let firstChar = Number(ship[i].toString().slice(0,1));
         let secondChar = Number(ship[i].toString().slice(-1));
         if (ship[i].toString().length === 1) {firstChar = 0}
-        newBoard[firstChar][secondChar] = "hide";
+        newBoard[firstChar][secondChar] = ["hide",ship];
       }
       return newBoard
     }
     let firstChar = Number(startOfShip.toString().slice(0,1));
     let secondChar = Number(startOfShip.toString().slice(-1));
-    if (newBoard[firstChar][secondChar] != null) {newBoard = this.randomShip(size,newBoard);return newBoard}
+    if (newBoard[firstChar][secondChar][0] != null) {newBoard = this.randomShip(size,newBoard);return newBoard}
     if (direction === 0 &&  firstChar > 10-size) {newBoard = this.randomShip(size,newBoard);return newBoard}
       if (direction === 1 &&  secondChar > 10-size) {newBoard = this.randomShip(size,newBoard);return newBoard}
       let ship = [];
@@ -69,7 +63,7 @@ constructor(props) {
     for (let i = 0; i < ship.length; i++) {
       let firstChar = Number(ship[i].toString().slice(0,1));
       let secondChar = Number(ship[i].toString().slice(-1));
-      newBoard[firstChar][secondChar] = "hide";
+      newBoard[firstChar][secondChar] = ["hide",ship];
     }
       return newBoard
   }
@@ -78,88 +72,88 @@ constructor(props) {
     for (let i = 0; i < ship.length; i++) {
       let current =  ship[i];
       let firstChar = Number(current.toString().slice(0,1));
-        let lastChar = Number(current.toString().slice(-1));
-        if (ship[i].toString().length === 1) {
-          if (ship[i] === 0) {
+      let lastChar = Number(current.toString().slice(-1));
+      if (ship[i].toString().length === 1) {
+        if (ship[i] === 0) {
           for (let j = 0; j < 2; j++) {
-                for (let k = 0; k < 2; k++) {
-                  let cur = current+j*10+k;
-                  if (cur.toString().length === 1) {
-                    firstChar = 0;
-                  }
-                  if (newBoard[firstChar+j][lastChar+k] != null) {
-                    return false
-                  }
-                  if (cur === current) {continue}
-                }
+            for (let k = 0; k < 2; k++) {
+              let cur = current+j*10+k;
+              if (cur.toString().length === 1) {
+                firstChar = 0;
               }
-          continue
-        }
-        if (ship[i] === 9) {
-          for (let j = 0; j < 2; j++) {
-                for (let k = -1; k < 1; k++) {
-                  let cur = current+j*10+k;
-                  if (cur.toString().length === 1) {
-                    firstChar = 0;
-                  }
-                  if (newBoard[firstChar+j][lastChar+k] != null) {
-                    return false
-                  }
-                  if (cur === current) {continue}
-                }
-              }
-          continue
-        }
-        for (let j = 0; j < 2; j++) {
-              for (let k = -1; k < 2; k++) {
-                let cur = current+j*10+k;
-                if (cur.toString().length === 1) {
-                  firstChar = 0;
-                }
-                if (newBoard[firstChar+j][lastChar+k] != null) {
-                  return false
-                }
-                if (cur === current) {continue}
-              }
-            }
-            continue
-        }
-      if (lastChar === 0) {
-        for (let j = -1; j < 2; j++) {
-              for (let k = 0; k < 2; k++) {
-                let cur = (firstChar+j)*10+(lastChar+k);
-                if (cur > 99) {continue}
-                if (newBoard[firstChar+j][lastChar+k] != null) {
-                  return false
-                }
-                if (cur === current) {continue}
-              }
-            }
-            continue
-      }
-      if (lastChar === 9) {
-        for (let j = -1; j < 2; j++) {
-              for (let k = -1; k < 1; k++) {
-                let cur = (firstChar+j)*10+(lastChar+k);
-                if (cur > 99) {continue}
-                if (newBoard[firstChar+j][lastChar+k] != null) {
-                  return false
-                }
-                if (cur === current) {continue}
-              }
-            }
-            continue
-      }
-      for (let j = -1; j < 2; j++) {
-            for (let k = -1; k < 2; k++) {
-              let cur = (firstChar+j)*10+(lastChar+k);
-              if (cur > 99) {continue}
-              if (newBoard[firstChar+j][lastChar+k] != null) {
+              if (newBoard[firstChar+j][lastChar+k][0] != null) {
                 return false
               }
               if (cur === current) {continue}
             }
           }
+          continue
+        }
+        if (ship[i] === 9) {
+          for (let j = 0; j < 2; j++) {
+            for (let k = -1; k < 1; k++) {
+              let cur = current+j*10+k;
+              if (cur.toString().length === 1) {
+                firstChar = 0;
+              }
+              if (newBoard[firstChar+j][lastChar+k][0] != null) {
+                return false
+              }
+              if (cur === current) {continue}
+            }
+          }
+        continue
+        }
+        for (let j = 0; j < 2; j++) {
+          for (let k = -1; k < 2; k++) {
+            let cur = current+j*10+k;
+            if (cur.toString().length === 1) {
+              firstChar = 0;
+            }
+            if (newBoard[firstChar+j][lastChar+k][0] != null) {
+              return false
+            }
+            if (cur === current) {continue}
+          }
+        }
+        continue
+      }
+      if (lastChar === 0) {
+        for (let j = -1; j < 2; j++) {
+          for (let k = 0; k < 2; k++) {
+            let cur = (firstChar+j)*10+(lastChar+k);
+            if (cur > 99) {continue}
+            if (newBoard[firstChar+j][lastChar+k][0] != null) {
+              return false
+            }
+            if (cur === current) {continue}
+          }
+        }
+        continue
+      }
+      if (lastChar === 9) {
+        for (let j = -1; j < 2; j++) {
+          for (let k = -1; k < 1; k++) {
+            let cur = (firstChar+j)*10+(lastChar+k);
+            if (cur > 99) {continue}
+            if (newBoard[firstChar+j][lastChar+k][0] != null) {
+              return false
+            }
+            if (cur === current) {continue}
+          }
+        }
+        continue
+      }
+      for (let j = -1; j < 2; j++) {
+        for (let k = -1; k < 2; k++) {
+          let cur = (firstChar+j)*10+(lastChar+k);
+          if (cur > 99) {continue}
+          if (newBoard[firstChar+j][lastChar+k][0] != null) {
+            return false
+          }
+          if (cur === current) {continue}
+        }
+      }
     }
     return true
   }
@@ -167,7 +161,7 @@ constructor(props) {
   randomPositioning() {
     let newBoard1 = [];
     for (let i = 0; i < 10; i++) {
-      newBoard1.push(Array(10).fill(null));
+      newBoard1.push(Array(10).fill(Array()));
     }
     let b = 0;
     for (let i = 0; i < 10; i++) {
@@ -176,7 +170,7 @@ constructor(props) {
     }
     let newBoard2 = [];
     for (let i = 0; i < 10; i++) {
-      newBoard2.push(Array(10).fill(null));
+      newBoard2.push(Array(10).fill(Array()));
     }
     b = 0;
     for (let i = 0; i < 10; i++) {
@@ -195,7 +189,6 @@ constructor(props) {
         <h1>Random Numb App</h1>
         <div className="yourboard">
         <button onClick={() => this.randomPositioning()}>Random</button>
-        <button onClick={() => this.reset()}>Reset</button>
         <button onClick={() => {this.props.sendBoard(this.state.next,this.state.yourBoard,this.state.enemyBoard)}}>Battle</button>
         <div> {this.renderCells()} </div>
         </div>
